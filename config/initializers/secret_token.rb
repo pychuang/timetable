f4db7a80d8acd681c3f63016ccb13eec96b0e165
Po-Yu Chuang <ratbert.chuang@gmail.com>
2014-02-19 20:42:49 +0800

    rails new timetable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetable::Application.config.secret_token = '8b8f17e0762ff12fa30d5dcd8ec95ffc9df3b20098efa557f8db6e0149acbdfb49fc9e1ea43bafe92c571eb407660f05e5437fd86a17eea79b9b081fe2a1466d'
