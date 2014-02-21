fcde6b22f02a19985e45d621a495a3fc148de4fb
Po-Yu Chuang <ratbert.chuang@gmail.com>
2014-02-21 21:17:09 +0800

    connect trains to companies and stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,6 @@
 class Station < ActiveRecord::Base
   attr_accessible :company_id, :name
   belongs_to :company
+  has_many :originates, :class_name => 'Train', :foreign_key => 'origin_id'
+  has_many :destines, :class_name => 'Train', :foreign_key => 'destination_id'
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,3 +1,6 @@
 class Train < ActiveRecord::Base
   attr_accessible :clockwise, :company_id, :date, :destination_id, :name, :number, :origin_id, :rank
+  belongs_to :company
+  belongs_to :origin, :class_name => 'Station', :foreign_key => 'origin_id'
+  belongs_to :destination, :class_name => 'Station', :foreign_key => 'destination_id'
 end
