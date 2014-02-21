7265ced119e17cd5096e0063f3477205e3f59ad2
Po-Yu Chuang <ratbert.chuang@gmail.com>
2014-02-21 22:18:59 +0800

    connect travels to train and station

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,3 +1,6 @@
 class Travel < ActiveRecord::Base
   attr_accessible :arrive_at, :arrive_to_id, :depart_at, :depart_from_id, :train_id
+  belongs_to :train
+  belongs_to :depart_from, :class_name => 'Station', :foreign_key => 'depart_from_id'
+  belongs_to :arrive_to, :class_name => 'Station', :foreign_key => 'arrive_to_id'
 end
