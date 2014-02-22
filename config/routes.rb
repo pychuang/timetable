e56ce5c18d3fa64108897217262d52bee3c03ab6
Po-Yu Chuang <ratbert.chuang@gmail.com>
2014-02-23 00:18:03 +0800

    root to top#index

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ Timetable::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'top#index', :as => 'top'
 
   # See how all your routes lay out with "rake routes"
 
